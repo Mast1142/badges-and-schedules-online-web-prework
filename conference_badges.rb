8bd878e1b6d1356798ba3af43439ea48a97b4313
Samuel W Tong <ssamt1142@gmail.com>
2019-02-23 23:02:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,5 +21,5 @@ def assign_rooms(speakers)
 end
 
 def printer(speakers)
-  batch_badge_creator(speakers).each {|n| puts "Hello, my name is #{n}.chomp"}
+  batch_badge_creator(speakers).each {|n| puts "Hello, my name is #{n}"}
 end
